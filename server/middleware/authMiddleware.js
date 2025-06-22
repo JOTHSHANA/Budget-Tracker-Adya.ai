90fe5cb65e764dd080e4a7b8fc51de795b290b85
JOTHSHANA <jothshana123@gmail.com>
2025-06-22 10:38:47 +0530

    ...

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -3,7 +3,6 @@ const User = require("../models/User");
 
 const protect = async (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
-    console.log("Token:", token);
     if (!token) return res.status(401).json({ message: "No token provided" });
 
     try {
