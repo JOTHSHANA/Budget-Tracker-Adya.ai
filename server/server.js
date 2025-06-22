ce3d977c2ab5acb09928d30d2540f73986380fba
JOTHSHANA <jothshana123@gmail.com>
2025-06-22 11:33:16 +0530

    cors fix

--- server/server.js
+++ server/server.js
@@ -13,7 +13,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://budget-tracker-adyaai.netlify.app',
+    credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
@@ -26,7 +29,7 @@ app.use("/api/alerts", alertRoutes);
 app.get('/', (req, res) => {
     res.send('API running...');
 });
-  
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
